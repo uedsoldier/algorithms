5dfa67968fa121e89cb61c169c88b202e11ced0e
Roberto Parra <jroberto.parra@tortoise.dev>
2022-03-11 18:44:42 -0600

    Corregidos todos los algoritmos y agregadas pruebas

---
+++ CHECKSUM_8/checksum8.c
@@ -0,0 +1,54 @@
+/**
+ * @file checksum8.c
+ * @brief Librería de implementación de funciones de suma de verificación de 8 bits
+ * @author Ing. José Roberto Parra Trewartha
+ * @version 1.0
+*/
+
+#include "checksum8.h"
+
+/**
+ * @brief Función para cálculo de checksum de 8 bits mediante OR exclusiva (XOR)
+ * @param datos (void*) Apuntador a dato o estructura de datos para determinar su checksum XOR
+ * @param len (uint16_t) Cantidad de datos para realizar checksum
+ * @return Checksum de 8 bits mediante XOR
+*/
+uint8_t checksum8_XOR(void *datos, uint16_t len) {
+	uint8_t retval = 0;						//Valor de retorno
+	uint8_t *_buf = (uint8_t*)datos;		//Asignacion de datos con apuntador a uint8_t
+	for(uint16_t i=0 ; i != len; i++) {		//Ciclo iterativo para cálculo de checksum
+		retval ^= *_buf++;
+	}
+	return retval;
+}
+
+
+/**
+ * @brief Función para cálculo de checksum de 8 bits mediante mediante módulo 256
+ * @param datos (void*) Apuntador a dato o estructura de datos para determinar su checksum módulo 256
+ * @param len (uint16_t) Cantidad de datos para realizar checksum
+ * @return Checksum de 8 bits mediante módulo 256
+*/
+uint8_t checksum8_modulo256(void *datos, uint16_t len) {
+	uint8_t retval = 0;						//Valor de retorno
+	uint8_t *_buf = (uint8_t*)datos;		//Asignacion de datos con apuntador a uint8_t
+	for(uint16_t i=0 ; i != len; i++) {		//Ciclo iterativo para suma de bytes
+		retval += *_buf++;
+	}
+	return retval;
+}
+
+/**
+ * @brief Función para cálculo de checksum de 8 bits mediante mediante complemento a 2
+ * @param datos (void*) Apuntador a dato o estructura de datos para determinar su checksum complemento a 2
+ * @param len (uint16_t) Cantidad de datos para realizar checksum
+ * @return Checksum de 8 bits mediante complemento a 2
+*/
+uint8_t checksum8_2complement(void *datos, uint16_t len) {
+	uint8_t retval = 0;						//Valor de retorno
+	uint8_t *_buf = (uint8_t*)datos;		//Asignacion de datos con apuntador a uint8_t
+	for(uint16_t i=0 ; i != len; i++) {		//Ciclo iterativo para suma de bytes
+		retval += *_buf++;
+	}
+	return (uint8_t)(0x0100-(uint8_t)(retval));
+}
--- CHECKSUM_8/checksum8.h
+++ CHECKSUM_8/checksum8.h
@@ -4,8 +4,10 @@
  * @author Ing. José Roberto Parra Trewartha
  * @version 1.0
 */
-#ifndef CHECKSUM_H
-#define CHECKSUM_H
+#ifndef CHECKSUM8_H
+#define CHECKSUM8_H
+
+#define CHECKSUM8_USE_IMPLEMENTATION_NAMES  0
 
 // Dependencias
 #include <stdint.h>
@@ -14,4 +16,13 @@ uint8_t checksum8_XOR(void *datos, uint16_t len);			// Función para cálculo de
 uint8_t checksum8_modulo256(void *datos, uint16_t len);		// Función para cálculo de checksum de 8 bits mediante módulo 256
 uint8_t checksum8_2complement(void *datos, uint16_t len);	// Función para cálculo de checksum de 8 bits mediante complemento a 2
 
-#endif /*CHECKSUM_H*/
+#if defined(CHECKSUM8_USE_IMPLEMENTATION_NAMES) && ( CHECKSUM8_USE_IMPLEMENTATION_NAMES == 1 )
+static const char checksum8_implementation_0[] = "XOR"; 
+static const char checksum8_implementation_1[] = "modulo 256";
+static const char checksum8_implementation_2[] = "2's complement";
+static const char * const checksum8_implementations[] = {
+    checksum8_implementation_0, checksum8_implementation_1, checksum8_implementation_2
+}
+#endif
+
+#endif /*CHECKSUM8_H*/
