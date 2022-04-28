b175d5a0a39a1fe252db18582ced38292d048a31
Roberto Parra <jroberto.parra@tortoise.dev>
2022-04-27 22:12:04 -0500

    XTEA CBC OK

--- XTEA/XTEA.h
+++ XTEA/XTEA.h
@@ -18,29 +18,13 @@
  * 
  */
 #ifndef XTEA_LOG
-#define XTEA_LOG 1
+#define XTEA_LOG 0
 #endif
-
-/**
- * @brief  Número de interaciones para la ejecución del algoritmo. 32 es bastante, 16 son suficientes, y alrededor de 8 serviría para muchas aplicaciones.
-*/
-#define XTEA_ITERATIONS 16
-
 
 /**
  * @brief Constante δ derivada de la razón áurea
 */
 #define XTEA_DELTA 0x9E3779B9UL
-
-/**
- * @brief Suma inicial para desencriptado
- * 0x8DDE6E40 para 64
- * 0xC6EF3720 para 32
- * 0xE3779B90 para 16
- * 0xF1BBCDC8 para 8
- * 
-*/
-static uint32_t XTEA_DEC_SUM = XTEA_DELTA * XTEA_ITERATIONS;
 
 /**
  * @brief 
@@ -53,15 +37,34 @@ typedef struct xtea_key{
     };
 } xtea_key_t;
 
+typedef struct XTEA{
+    uint32_t dec_sum;
+    /**
+     * Suma inicial para desencriptado
+     * 0x1BBCDC80 para 128 iteraciones
+     * 0x8DDE6E40 para 64 iteraciones
+     * 0xC6EF3720 para 32 iteraciones
+     * 0xE3779B90 para 16 iteraciones
+     * 0xF1BBCDC8 para 8 iteraciones
+     */   
+    uint32_t iterations;    // Número de iteraciones para la ejecución del algoritmo. 32 es bastante, 16 son suficientes, y alrededor de 8 serviría para muchas aplicaciones. 
+    xtea_key_t key;         // Llave del algoritmo, consistente en 128 bits(4 enteros de 32 bits).
+    uint8_t iv[8];          // Vector de inicialización para modalidad CBC
+} XTEA_t;
+
+typedef enum {
+    XTEA_8, XTEA_16, XTEA_32, XTEA_64
+} XTEA_rounds_t;
 
 /**
  * Prototipos de funciones
 */
-static void XTEA_encrypt_chunk(uint32_t *in, uint32_t *out, xtea_key_t *key );  // Función de cifrado de trozo de 8 bytes
-static void XTEA_decrypt_chunk(uint32_t * in,uint32_t * out, xtea_key_t *key);  // Función de descrifrado de trozo de 8 bytes
+int8_t XTEA_init(XTEA_t *xtea, XTEA_rounds_t rounds, xtea_key_t *key, uint8_t *iv);
+static void XTEA_encrypt_chunk(XTEA_t *xtea, uint32_t *in, uint32_t *out);  // Función de cifrado de trozo de 8 bytes
+static void XTEA_decrypt_chunk(XTEA_t *xtea, uint32_t * in,uint32_t * out);  // Función de descrifrado de trozo de 8 bytes
 
-int32_t XTEA_encrypt(void *in, void *out, int32_t size, xtea_key_t *key);
-int32_t XTEA_decrypt(void *in, void *out, int32_t size, xtea_key_t *key);
+int32_t XTEA_encrypt(XTEA_t *xtea, void *in, void *out, int32_t size, bool ecb);
+int32_t XTEA_decrypt(XTEA_t *xtea, void *in, void *out, int32_t size, bool ecb);
 
 
 #endif /*XTEA_H*/
--- XTEA/test.c
+++ XTEA/test.c
@@ -7,34 +7,47 @@
 char cadena[128];
 char buffer1[128];
 char buffer2[128];
+// uint8_t iv[] = {0x22,0xBB,0xCC,0xDD,0xEE,0xFF,0x55,0x33};
+uint8_t iv[] = {'U','x','G','Z','V','F','r','T'};
 
 
 xtea_key_t xtea_test_key;
+XTEA_t xtea_test;
 
 
 int main(int argc, char *argv[]){
-    if (argc != 3)
+    if (argc != 5)
     {
-        printf("Se deben mandar dos argumentos: Cadena a cifrar y cadena de llave...");
+        printf("Se deben mandar 4 argumentos: Cadena a cifrar, cadena de llave, bandera cbc y numero de rondas XTEA...");
         return EXIT_FAILURE;
     }
+    
     memset(cadena,0,sizeof(cadena));
     memset(buffer1,0,sizeof(buffer1));
     memset(buffer2,0,sizeof(buffer2));
+    memset(&xtea_test_key,0,sizeof(xtea_key_t));
     sprintf(cadena,"%s",argv[1]);
     uint16_t str_len = strlen(argv[1]);
     uint16_t key_len =  strlen(argv[2]);
-    memcpy(xtea_test_key.key_bytes, argv[2],key_len);
-    
-    printf("ASCII in: %s. ASCII input length: %u\r\n",argv[1],str_len);
-    printf("XTEA key: %s. Key size: %u\r\n",xtea_test_key.key_bytes, key_len);
-    XTEA_encrypt(cadena,buffer1,str_len,&xtea_test_key);
-    
-    // XTEA_crypt_chunk((uint32_t *)cadena,(uint32_t *)buffer1, &xtea_test_key);
-    printf("XTEA: %s\r\n",buffer1);
+    bool ecb = (strcmp(argv[3],"1"))==0? true:false;
+    uint32_t rounds = strtol(argv[4],NULL,10);
+    memcpy(&xtea_test_key, argv[2], key_len);
+    XTEA_init( &xtea_test, rounds, &xtea_test_key,iv);
 
-    XTEA_decrypt(buffer1,buffer2,str_len,&xtea_test_key);
-    // XTEA_decrypt_chunk((uint32_t *)buffer1,(uint32_t *)buffer2, &xtea_test_key);
+    printf("ASCII in: %s\nASCII input length: %u\n",argv[1],str_len);
+    printf("ECB mode: %s\n",ecb? "true":"false");
+    printf("Rounds: %lu\n",rounds);
+
+
+    uint32_t enc_chunks = XTEA_encrypt(&xtea_test, cadena,buffer1,str_len,ecb);
+    
+    printf("XTEA output: ");
+    for (size_t i = 0; i != str_len; i++){
+        printf("%02x ",(uint8_t)buffer1[i]);
+    }
+    printf("\n");
+    
+    XTEA_decrypt(&xtea_test, buffer1,buffer2,str_len,ecb);
     printf("De vuelta a ASCII: %s",buffer2);
     return 0;
 }
