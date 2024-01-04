9caa77b3e1066bcfc089a3530d1406668d773212
Roberto Parra <uedsoldier1990@gmail.com>
2024-01-03 21:43:53 -0600

    Automated test binaries generator script OK. Only works with CMake and MinGW in windows for now

--- tests/generate_test_binaries.py
+++ tests/generate_test_binaries.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
+from os import chdir,getcwd,system
+from shutil import rmtree
 
-algorithm_choices = ('base64','crc','xtea','checksum')
+algorithm_choices = ('BASE64','CRC','XTEA','CHECKSUM8')
 
 
 
@@ -17,8 +19,13 @@ def main():
     )
     args = CLI.parse_args()
     algorithm = args.algorithm[0]
-    print(f'Algorithm: {algorithm}')    
-
+    print(f'Algorithm: {algorithm}')
+    chdir(algorithm)
+    print(getcwd())
+    rmtree('build/',ignore_errors=True)
+    system('cmake -S . -B build/ -G "MinGW Makefiles"')
+    chdir('build')
+    system('mingw32-make')
 
 if __name__ == '__main__':
     main()
