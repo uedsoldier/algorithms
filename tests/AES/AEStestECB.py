91544beda1f1741ccdf04ec253b753624935f7e1
Roberto Parra <uedsoldier1990@gmail.com>
2024-01-08 00:05:10 -0600

    test ECB on the way

--- tests/AES/AEStestECB.py
+++ tests/AES/AEStestECB.py
@@ -1,9 +1,21 @@
 import pyaes, pbkdf2, binascii, os, secrets, sys
 
-# Derive a 256-bit AES encryption key from the password
+# Derive a AES encryption key from the password
 password = sys.argv[1]
-passwordSalt = os.urandom(16)
-key = pbkdf2.PBKDF2(password, passwordSalt).read(16)
+aes_std = sys.argv[2]
+
+match aes_std:
+    case 'AES-128':
+        KEY_LEN = 16
+    case 'AES-192':
+        KEY_LEN = 24
+    case 'AES-256':
+        KEY_LEN = 32
+    case _:
+        raise ValueError('Invalid AES KEY_LEN')
+
+passwordSalt = os.urandom(KEY_LEN)
+key = pbkdf2.PBKDF2(password, passwordSalt).read(KEY_LEN)
 
 
 plaintext = 'HolaHolaHola\0\0\0\0'
