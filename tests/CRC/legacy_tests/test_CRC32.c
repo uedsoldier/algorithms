325052a194354b46695bb5d9d747e38f9168e509
Roberto Parra <uedsoldier1990@gmail.com>
2025-03-21 15:51:57 -0600

    Add legacy CRC tests for CRC8, CRC16, and CRC32 implementations

---
+++ tests/CRC/legacy_tests/test_CRC32.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "crc.h"
+
+const char *TEST_NAME = "CRC32 test";
+const char *TEST_STRING = "Test string for CRC validation";
+uint16_t test_str_len;
+uint8_t impl_cnt = 0;
+
+const uint32_t test_CRC32_values[] = {
+    0x6ABDD5BA,  // CRC32_D
+    0x26374F09,  // CRC32_Q
+    0x44E584B8,  // CRC32_C
+    0xFA2E39F4,  // CRC32_ISO
+    0xFF368778,  // CRC32_BZIP2
+    0x00C97887,  // CRC32_MPEG_2
+    0xADB55887,  // CRC32_POSIX
+    0x05D1C60B,  // CRC32_JAMCRC
+    0xA2761C90,  // CRC32_XFER
+};
+
+int main(int argc, char *argv[]) {
+    bool crc_equal;
+    printf("%s\n", TEST_NAME);
+    test_str_len = strlen(TEST_STRING);
+    printf("ASCII input: <<%s>> (length: %u bytes)\n", TEST_STRING,
+           test_str_len);
+
+    for (crc_t i = CRC32_D; i <= CRC32_XFER; i++) {
+        uint32_t result = CRC32(TEST_STRING, test_str_len, i);
+        crc_equal = (result == test_CRC32_values[impl_cnt]);
+        printf("Algorithm: %-22s\n", get_crc16_implementation(impl_cnt));
+        printf(" * Result: 0x%02X  Expected: 0x%02X\n", result,
+               test_CRC32_values[impl_cnt]);
+        if (!crc_equal) {
+            break;
+        }
+        impl_cnt++;
+    }
+
+    printf("%s %s\n", TEST_NAME, crc_equal ? "OK" : "not OK");
+    return crc_equal ? EXIT_SUCCESS : EXIT_FAILURE;
+}
---
+++ tests/CRC/legacy_tests/test_CRC8.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "crc.h"
+
+const char *TEST_NAME = "CRC8 test";
+const char *TEST_STRING = "Test string for CRC validation";
+uint16_t test_str_len;
+uint8_t impl_cnt = 0;
+
+const uint8_t test_CRC8_values[] = {
+    0xDC,  // CRC8_CCITT
+    0xF8,  // CRC8_CDMA2000
+    0xCD,  // CRC8_DARC
+    0x34,  // CRC8_DVB_S2
+    0x18,  // CRC8_EBU
+    0x34,  // CRC8_I_CODE
+    0x89,  // CRC8_ITU
+    0xD0,  // CRC8_MAXIM
+    0x07,  // CRC8_ROHC
+    0xF1,  // CRC8_WCDMA
+    0x93,  // CRC8_SAE_J1850
+    0x8A,  // CRC8_SAE_J1850_ZERO
+    0x62,  // CRC8_8H2F
+};
+
+int main(int argc, char *argv[]) {
+    bool crc_equal;
+    printf("%s\n", TEST_NAME);
+    test_str_len = strlen(TEST_STRING);
+    printf("ASCII input: <<%s>> (length: %u bytes)\n", TEST_STRING,
+           test_str_len);
+
+    for (crc_t i = CRC8_CCITT; i <= CRC8_8H2F; i++) {
+        uint8_t result = CRC8(TEST_STRING, test_str_len, i);
+        crc_equal = (result == test_CRC8_values[impl_cnt]);
+        printf("Algorithm: %-22s\n", get_crc8_implementation(impl_cnt));
+        printf(" * Result: 0x%02X  Expected: 0x%02X\n", result,
+               test_CRC8_values[impl_cnt]);
+        if (!crc_equal) {
+            break;
+        }
+        impl_cnt++;
+    }
+
+    printf("%s %s\n", TEST_NAME, crc_equal ? "OK" : "not OK");
+    return crc_equal ? EXIT_SUCCESS : EXIT_FAILURE;
+}
