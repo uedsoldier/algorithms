912cfda2b999e631999e616683e136eda7c9eb34
Roberto Parra <uedsoldier1990@gmail.com>
2025-03-30 15:13:22 -0600

    Refactor checksum8 test files for improved readability and consistency

--- tests/CHECKSUM8/legacy_tests/test_checksum8_2scomplement.c
+++ tests/CHECKSUM8/legacy_tests/test_checksum8_2scomplement.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <stdint.h>
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include "../../src/CHECKSUM8/checksum8.h"
+#include <string.h>
+
+#include "checksum8.h"
 
 const char *TEST_STRING = "test checksum8";
 const char *TEST_NAME = "checksum8-2complement";
@@ -13,17 +14,22 @@ const uint8_t expected_checksum8_2complement = 0x95;
 #define MAX_INPUT_STRING_SIZE 32
 char input_string[MAX_INPUT_STRING_SIZE];
 
-int main(int argc, char *argv[])
-{
-    printf("%s\n",TEST_NAME);
+int main(int argc, char *argv[]) {
+    printf("%s\n", TEST_NAME);
     uint16_t input_str_len = strlen(TEST_STRING);
     memset(input_string, 0, MAX_INPUT_STRING_SIZE);
-    memcpy(input_string,TEST_STRING,input_str_len);
-    printf("Input string: <<%s>> Size: %u bytes\n", input_string, input_str_len);
-    
-    uint8_t checksum = checksum8(input_string, sizeof(input_string), checksum_type);
-    printf("Checksum %s: 0x%02X - Expected: 0x%02X\n", checksum8_implementations[checksum_type], checksum, expected_checksum8_2complement);
+    memcpy(input_string, TEST_STRING, input_str_len);
+    printf("Input string: <<%s>> Size: %u bytes\n", input_string,
+           input_str_len);
 
-    printf("%s %s",TEST_NAME, checksum == expected_checksum8_2complement? "OK":"not OK");
-    return checksum == expected_checksum8_2complement? EXIT_SUCCESS:EXIT_FAILURE;
+    uint8_t checksum =
+        checksum8(input_string, sizeof(input_string), checksum_type);
+    printf("Checksum %s: 0x%02X - Expected: 0x%02X\n",
+           checksum8_implementations[checksum_type], checksum,
+           expected_checksum8_2complement);
+
+    printf("%s %s", TEST_NAME,
+           checksum == expected_checksum8_2complement ? "OK" : "not OK");
+    return checksum == expected_checksum8_2complement ? EXIT_SUCCESS
+                                                      : EXIT_FAILURE;
 }
